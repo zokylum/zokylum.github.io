86d288baa1fde3696835f929588f2487e5c49df4
zokylum <105221906+zokylum@users.noreply.github.com>
2022-10-15 16:10:16 +0200

    new site

---
+++ Robin site/app.js
@@ -0,0 +1,17 @@
+/*let sun = document.getElementById("switch").display;
+let moon = document.getElementById("switchy").display;
+let link = document.getElementById("linkyBoah").href;*/
+
+function light() {
+  document.getElementById("linkyBoah").href = "./style/light.css";
+  document.getElementById("switch").style.display = "none";
+  document.getElementById("switchy").style.display = "inline";
+  console.log("hi");
+}
+
+function dark() {
+  document.getElementById("linkyBoah").href = "./style/style.css";
+  document.getElementById("switch").style.display = "inline";
+  document.getElementById("switchy").style.display = "none";
+  console.log("yo");
+}
