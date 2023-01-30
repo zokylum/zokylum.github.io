7d61a3d79b4b3d7f9abf04cee816e1e3e5c694e0
zokylum <105221906+zokylum@users.noreply.github.com>
2023-01-30 22:28:51 +0100

    img on site

--- Robin.site/app.js
+++ Robin.site/app.js
@@ -4,14 +4,14 @@ let link = document.getElementById("linkyBoah").href;*/
 
 function light() {
   document.getElementById("linkyBoah").href = "./style/light.css";
-  document.getElementById("switch").style.display = "none";
-  document.getElementById("switchy").style.display = "inline";
+  //document.getElementById("switch").style.display = "none";
+  //document.getElementById("switchy").style.display = "inline";
   console.log("hi");
 }
 
 function dark() {
   document.getElementById("linkyBoah").href = "./style/style.css";
-  document.getElementById("switch").style.display = "inline";
-  document.getElementById("switchy").style.display = "none";
+  //document.getElementById("switch").style.display = "inline";
+  //document.getElementById("switchy").style.display = "none";
   console.log("yo");
 }
